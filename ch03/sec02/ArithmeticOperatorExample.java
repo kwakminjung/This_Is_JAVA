8c9f529a982e6a529230260ec63ae083cf5ed00c
kwakminjung <kwak1126mj@gmail.com>
2023-01-13 22:50:08 +0900

    chapter03

---
+++ ch03/sec02/ArithmeticOperatorExample.java
@@ -0,0 +1,25 @@
+package ch03.sec02;
+
+public class ArithmeticOperatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte v1 = 10;
+		byte v2 = 4;
+		int v3 = 5;
+		long v4 = 10L;
+		
+		int result1 = v1 + v2;
+		System.out.println("result1: " + result1); //result1: 14
+		
+		long result2 = v1 + v2 - v4;
+		System.out.println("result2: " + result2); //result2: 4
+		
+		double result3 = (double)v1 / v2;
+		System.out.println("result3: " + result3); //result3: 2.5
+		
+		int result4 = v1 % v2;
+		System.out.println("result4: " + result4); //result4: 2
+	}
+
+}

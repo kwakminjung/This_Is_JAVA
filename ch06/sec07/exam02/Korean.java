0eddf6ab0faabfbd8f6c58957da5e05e9c3fb435
kwakminjung <kwak1126mj@gmail.com>
2023-01-30 23:44:00 +0900

    chapter06.클래스(2)

--- ch06/sec07/exam02/Korean.java
+++ ch06/sec07/exam02/Korean.java
@@ -6,8 +6,11 @@ public class Korean {
 	String name;
 	String ssn;
 	//생성자 선언
-	public Korean(String n, String s) {
-		name = n;
-		ssn = s;
+	public Korean(String name, String ssn) {
+		//가독성을 위해 필드명과 매개변수명을 동일하게 함
+		//-> 필드와 매개변수의 이름이 같으므로 this키워드 써야함
+		//이클립스에서 필드는 파란색, 매개변수는 갈색임
+		this.name = name;
+		this.ssn = ssn;
 	}
 }
